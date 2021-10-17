65287c095f4aa4101642e721b92f0de0971947a7
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-18 01:14:14 +0600

    firebase config and initialize files created

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRnhhJPhDA51xFYudONeOh1D5mU-J_yRs",
+    authDomain: "healthcare-website-samimeadad.firebaseapp.com",
+    projectId: "healthcare-website-samimeadad",
+    storageBucket: "healthcare-website-samimeadad.appspot.com",
+    messagingSenderId: "732008807405",
+    appId: "1:732008807405:web:0091d6428d4f07678685e4"
+};
+
+export default firebaseConfig;
